d25dee09a29b3fa26daed447d4ae003fed3a036c
Alizon Kingslen <kingslenalizon@icloud.com>
2026-02-26 12:09:04 -0400

    feat: header of the homepage

--- components/navList.tsx
+++ components/navList.tsx
@@ -3,16 +3,42 @@ import Link from "next/link";
 
 export default function NavList() {
     return (
-        <header className="my-5">
+        <header className="my-5 flex items-center justify-between border-b-2 border-gray-400">
             <Link href="/">
                 <Image
-                    src="/treinner.png"
-                    width={200}
-                    height={200}
+                    src="/gladiator-helmet96.png"
+                    width={75}
+                    height={75}
                     alt="Logo"
                     className="rounded-full"
                 />
             </Link>
+            <nav className="flex gap-3 text-lg ">
+                <Link
+                    href="/products"
+                    className="hover:underline hover:font-bold hover:scale-3d transition-all  duration-100"
+                >
+                    Products
+                </Link>
+                <Link
+                    href="/about"
+                    className="hover:underline hover:font-bold hover:scale-3d transition-all duration-100"
+                >
+                    About
+                </Link>
+                <Link
+                    href="/contact"
+                    className="hover:underline hover:font-bold hover:scale-3d transition-all duration-100"
+                >
+                    Contact
+                </Link>
+                <Link
+                    href="/login"
+                    className="hover:underline hover:font-bold hover:scale-3d transition-all duration-100"
+                >
+                    Login
+                </Link>
+            </nav>
         </header>
     );
 }
